12fb28c383b7282cf071ae168987e79c445ddfdc
Samir Schabel <sschabel@p-und-c.de>
2023-02-17 10:27:20 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [time, setTime] = useState(new Date());
   const [date, setDate] = useState(new Date());
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  function showDate(){
-    setDate(new Date())
+  function showTime(){
+    setTime(new Date())
   }
 
   useEffect(() => {
-    const timerId = setInterval(showDate, 1000);
+    const timerId = setInterval(showTime, 1000);
     return function cleanUp(){
       clearInterval(timerId)
     }
@@ -17,8 +19,11 @@ function App() {
 
   return (
     <>
-    <span>
-    {date.toLocaleTimeString()}
+    <span className='date'>
+      {date.toLocaleDateString('de-DE', options)}
+    </span>
+    <span className='time'>
+    {time.toLocaleTimeString()}
     </span>
     </>
   )
